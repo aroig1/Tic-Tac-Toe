6891c345beaa72f78a43d1d1f7fc1d3c4f672717
aroig1 <aroig@arizona.edu>
2023-05-10 18:16:24 -0700

    Switched buttons into 2D array

--- GameGUI.py
+++ GameGUI.py
@@ -13,39 +13,24 @@ class gameGUI:
         self.label.pack(pady=40)
 
         self.gameFrame = tk.Frame(self.root, background="Black", borderwidth=2)
-        self.gameFrame.columnconfigure(0, weight=2)
-        self.gameFrame.columnconfigure(1, weight=2)
-        self.gameFrame.columnconfigure(2, weight=2)
+        self.gameFrame.columnconfigure(0, weight=1)
+        self.gameFrame.columnconfigure(1, weight=1)
+        self.gameFrame.columnconfigure(2, weight=1)
         self.gameFrame.pack(padx=20, pady=50, fill='x')
 
-        self.btn1 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
-
-        self.btn2 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
-
-        self.btn3 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
-
-        self.btn4 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
-
-        self.btn5 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
-
-        self.btn6 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
-
-        self.btn7 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn7.grid(row=2, column=0, sticky=tk.W+tk.E)
-
-        self.btn8 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn8.grid(row=2, column=1, sticky=tk.W+tk.E)
-
-        self.btn9 = tk.Button(self.gameFrame, text="", font=('Arial', 25))
-        self.btn9.grid(row=2, column=2, sticky=tk.W+tk.E)
+        self.btn = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        for x in range(3):
+            for y in range(3):
+                self.btn[x][y] = tk.Button(self.gameFrame, text="", font=('Arial', 25))
+                self.btn[x][y].grid(row=x, column=y, sticky=tk.W+tk.E)
 
         self.root.protocol("WM_DELETE_WINDOW", self.root.destroy)
+
+        turnLabel = tk.Label(self.root, text="It is ___ turn", font=('Arial', 20))
+        turnLabel.pack(pady=20)
+
+        errorMessage = tk.Label(self.root, text="Insert turn error here", font=('Arial', 20))
+        errorMessage.pack(pady=20)
 
         self.root.mainloop()
 
