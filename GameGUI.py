aba8443236cbe8edf4caf77280c3617835174ffb
aroig1 <aroig@arizona.edu>
2023-05-12 14:27:41 -0700

    Added Button Functionality

--- GameGUI.py
+++ GameGUI.py
@@ -6,6 +6,8 @@ class gameGUI:
         
         self.root = tk.Tk()
 
+        self.gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]]
+
         self.root.geometry("600x700")
         self.root.title("Tic-Tac-Toe")
 
@@ -18,10 +20,11 @@ class gameGUI:
         self.gameFrame.columnconfigure(2, weight=1)
         self.gameFrame.pack(padx=20, pady=50, fill='x')
 
-        self.btn = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        self.btn = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
         for x in range(3):
             for y in range(3):
-                self.btn[x][y] = tk.Button(self.gameFrame, text="", font=('Arial', 25))
+                self.btn[x][y] = tk.Button(self.gameFrame, text=self.gameBoard[x][y], font=('Arial', 25), command=lambda x=x, y=y : self.buttonPressed(x, y))
                 self.btn[x][y].grid(row=x, column=y, sticky=tk.W+tk.E)
 
         self.root.protocol("WM_DELETE_WINDOW", self.root.destroy)
@@ -33,5 +36,14 @@ class gameGUI:
         errorMessage.pack(pady=20)
 
         self.root.mainloop()
+
+    def checkForPress():
+
+        pass
+
+
+    def buttonPressed(self, row, col):
+         self.btn[row][col]['text'] = 'X'
+         pass
 
 gameGUI()
